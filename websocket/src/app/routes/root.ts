83a6482f7447db59a9d8a74fe3a69c094bdccaeb
Tal_Shmuel_Kleiner <talkl120@gmail.com>
2025-06-27 14:04:47 +0300

    add docker compose for postgres, add dev setup shell script, add prisma as package

--- websocket/src/app/routes/root.ts
+++ websocket/src/app/routes/root.ts
@@ -1,4 +1,6 @@
 import { FastifyInstance } from 'fastify';
+import { db } from '@launch-pad/rdb'
+
 
 export default async function (fastify: FastifyInstance) {
   fastify.get('/', async function () {
